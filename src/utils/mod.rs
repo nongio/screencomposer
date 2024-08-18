b57747bdbb54728ec20d61ed23c1b4311a7196b2
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2024-08-18 12:24:22 +0200

    remove bin pack dependency

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -34,50 +34,6 @@ pub fn image_from_path(image_path: &str) -> Option<skia_safe::Image> {
     Some(image)
 }
 
-pub fn bin_pack(windows: &Vec<crate::workspace::Window>, bin_width: f32, bin_height: f32) -> Box<dyn binpack2d::BinPacker>
-{
-    let total_window_area: f32 = {
-        windows.iter()
-            .map(|window| {
-                window.w * window.h
-            })
-            .sum()
-    };
-
-    let total_bin_area = bin_width * bin_height;
-    let mut scale_factor = (total_bin_area / total_window_area).sqrt();
-    let mut items_to_place = Vec::new();
-    windows.iter().for_each(|win| {
-        let (window_width, window_height) = (win.w, win.h);
-        let id = win.base_layer.id().unwrap();
-        let id: usize = id.0.into();
-        let dimension = binpack2d::Dimension::with_id(id as isize, (window_width * scale_factor) as i32, (window_height * scale_factor) as i32, 20);
-        items_to_place.push(dimension);
-    });
-
-    let mut bin = binpack2d::bin_new(binpack2d::BinType::MaxRects, bin_width as i32, bin_height as i32);
-    let (mut inserted, mut rejected) = bin.insert_list(&items_to_place);
-    let mut tries = 0;
-    while (!rejected.is_empty() || inserted.len() != windows.len()) && tries < 100 {
-        scale_factor *= 0.99;
-        scale_factor = scale_factor.max(0.1);
-        let mut items_to_place = Vec::new();
-        windows.iter().for_each(|win| {
-            let (window_width, window_height) = (win.w, win.h);
-
-            let id = win.base_layer.id().unwrap();
-            let id: usize = id.0.into();
-            let dimension = binpack2d::Dimension::with_id(id as isize, (window_width * scale_factor) as i32, (window_height * scale_factor) as i32, 20);
-            items_to_place.push(dimension);
-        });
-        bin.clear();
-        (inserted, rejected) = bin.insert_list(&items_to_place);
-        tries += 1;
-    }
-
-    bin
-}
-
 pub trait Observable<T> {
     fn add_listener(&mut self, observer: std::sync::Arc<dyn Observer<T>>);
     fn observers<'a>(&'a self) -> Box<dyn Iterator<Item = std::sync::Weak<dyn Observer<T>>> + 'a>;
