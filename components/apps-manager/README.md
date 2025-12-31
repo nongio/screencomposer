f2a7c3f50a7b280c32887dd441aa56ae52522b1f
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2025-12-31 12:17:54 +0100

    feat: apps-manager component init

---
+++ components/apps-manager/README.md
@@ -0,0 +1,61 @@
+# Apps - Foreign Toplevel List Debug Tool
+
+A simple command-line tool to test the `ext_foreign_toplevel_list_v1` protocol implementation in ScreenComposer.
+
+## Building
+
+```bash
+cargo build -p apps-manager
+```
+
+## Usage
+
+### Testing with ScreenComposer
+
+1. **Start ScreenComposer in one terminal:**
+   ```bash
+   cargo run -- --winit
+   ```
+
+2. **Run the debug tool in another terminal:**
+   ```bash
+   # Set the Wayland display if needed
+   export WAYLAND_DISPLAY=wayland-1  # or wayland-0, check ScreenComposer output
+   
+   # Run the tool
+   cargo run -p apps-manager
+   ```
+
+3. **Open some windows** in ScreenComposer (e.g., with sample clients or other applications)
+
+4. **Observe the output:**
+   The tool will print messages when:
+   - New toplevels are created
+   - Toplevel title/app_id changes are detected
+   - Toplevels are closed
+
+### Example Output
+
+```
+Foreign Toplevel List Debug Tool
+Connecting to Wayland compositor...
+
+Found ext_foreign_toplevel_list_v1 v1
+Waiting for events... (Press Ctrl+C to exit)
+
+New toplevel #0
+Toplevel #0: title='Terminal' app_id='org.gnome.Terminal' identifier='<none>'
+New toplevel #1
+Toplevel #1: title='Firefox' app_id='firefox' identifier='<none>'
+Toplevel #1 closed: Firefox (firefox)
+```
+
+## Protocol Details
+
+This tool implements a client for the `ext_foreign_toplevel_list_v1` Wayland protocol, which allows applications to:
+- Get a list of all open windows (toplevels)
+- Receive notifications when windows are created or destroyed
+- Get window properties (title, app_id, identifier)
+- Receive updates when window properties change
+
+This is the modern replacement for `wlr_foreign_toplevel_management_unstable_v1` and is used by task bars, docks, and window switchers.
---
+++ components/apps-manager/src/main.rs
@@ -0,0 +1,151 @@
+use std::collections::HashMap;
+use wayland_client::{protocol::wl_registry, Connection, Dispatch, QueueHandle, Proxy};
+use wayland_protocols::ext::foreign_toplevel_list::v1::client::{
+    ext_foreign_toplevel_handle_v1::{self, ExtForeignToplevelHandleV1},
+    ext_foreign_toplevel_list_v1::{self, ExtForeignToplevelListV1},
+};
+
+#[derive(Debug, Clone)]
+struct ToplevelInfo {
+    title: Option<String>,
+    app_id: Option<String>,
+    identifier: Option<String>,
+}
+
+struct AppState {
+    toplevels: HashMap<u32, ToplevelInfo>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            toplevels: HashMap::new(),
+        }
+    }
+}
+impl Dispatch<wl_registry::WlRegistry, ()> for AppState {
+    fn event(
+        _state: &mut Self,
+        registry: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        _: &(),
+        _: &Connection,
+        qh: &QueueHandle<Self>,
+    ) {
+        if let wl_registry::Event::Global {
+            name,
+            interface,
+            version,
+        } = event
+        {
+            match &interface[..] {
+                "ext_foreign_toplevel_list_v1" => {
+                    println!("Found ext_foreign_toplevel_list_v1 v{}", version);
+                    registry.bind::<ExtForeignToplevelListV1, _, _>(name, version.min(1), qh, ());
+                }
+                _ => {}
+            }
+        }
+    }
+}
+impl Dispatch<ExtForeignToplevelListV1, ()> for AppState {
+    fn event(
+        state: &mut Self,
+        _proxy: &ExtForeignToplevelListV1,
+        event: ext_foreign_toplevel_list_v1::Event,
+        _: &(),
+        _: &Connection,
+        _qh: &QueueHandle<Self>,
+    ) {
+        match event {
+            ext_foreign_toplevel_list_v1::Event::Toplevel { toplevel } => {
+                // Use the Wayland object ID as our ID
+                let id = toplevel.id().protocol_id();
+                println!("New toplevel #{}", id);
+                state.toplevels.insert(
+                    id,
+                    ToplevelInfo {
+                        title: None,
+                        app_id: None,
+                        identifier: None,
+                    },
+                );
+            }
+            ext_foreign_toplevel_list_v1::Event::Finished => {
+                println!("Foreign toplevel list finished");
+            }
+            _ => {}
+        }
+    }
+
+    wayland_client::event_created_child!(AppState, ExtForeignToplevelListV1, [
+        0 => (ExtForeignToplevelHandleV1, 0u32)
+    ]);
+}
+
+impl Dispatch<ExtForeignToplevelHandleV1, u32> for AppState {
+    fn event(
+        state: &mut Self,
+        proxy: &ExtForeignToplevelHandleV1,
+        event: ext_foreign_toplevel_handle_v1::Event,
+        _udata: &u32,
+        _: &Connection,
+        _qh: &QueueHandle<Self>,
+    ) {
+        // Use the proxy's Wayland object ID
+        let id = proxy.id().protocol_id();
+        match event {
+            ext_foreign_toplevel_handle_v1::Event::Closed => {
+                if let Some(info) = state.toplevels.remove(&id) {
+                    println!(
+                        "Toplevel #{} closed: {} ({})",
+                        id,
+                        info.title.as_deref().unwrap_or("<no title>"),
+                        info.app_id.as_deref().unwrap_or("<no app_id>")
+                    );
+                }
+            }
+            ext_foreign_toplevel_handle_v1::Event::Done => {
+                if let Some(info) = state.toplevels.get(&id) {
+                    println!(
+                        "Toplevel #{}: title='{}' app_id='{}' identifier='{}'",
+                        id,
+                        info.title.as_deref().unwrap_or("<none>"),
+                        info.app_id.as_deref().unwrap_or("<none>"),
+                        info.identifier.as_deref().unwrap_or("<none>")
+                    );
+                }
+            }
+            ext_foreign_toplevel_handle_v1::Event::Title { title } => {
+                if let Some(info) = state.toplevels.get_mut(&id) {
+                    info.title = Some(title);
+                }
+            }
+            ext_foreign_toplevel_handle_v1::Event::AppId { app_id } => {
+                if let Some(info) = state.toplevels.get_mut(&id) {
+                    info.app_id = Some(app_id);
+                }
+            }
+            ext_foreign_toplevel_handle_v1::Event::Identifier { identifier } => {
+                if let Some(info) = state.toplevels.get_mut(&id) {
+                    info.identifier = Some(identifier);
+                }
+            }
+            _ => {}
+        }
+    }
+}
+fn main() {
+    println!("Foreign Toplevel List Debug Tool");
+    println!("Connecting to Wayland compositor...\n");
+    let conn = Connection::connect_to_env().expect("Failed to connect to Wayland");
+    let display = conn.display();
+    let mut event_queue = conn.new_event_queue();
+    let qh = event_queue.handle();
+    let _registry = display.get_registry(&qh, ());
+    let mut state = AppState::new();
+    println!("Waiting for events... (Press Ctrl+C to exit)\n");
+    loop {
+        event_queue.blocking_dispatch(&mut state).unwrap();
+    }
+}
